47517d176f31e99af4385137f6407d19fb90f2ee
user <user@example.com>
2021-09-12 23:44:19 -0400

    Allow adding of custom options

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # simpleqemu
 Helper script for creating and booting qemu virtual machines without libvirt.<br  />
-Turns out someone already made this, though it seems to rely on qemu-virgil.<br  />
+Turns out someone already made this, though it is written in bash not POSIX shell.
+Also the source code uses spaces instead of real tabs which I find repulsive.<br  />
 [Quickemu](https://github.com/wimpysworld/quickemu)
 
 # Useful Links
+[Of Course the Arch Wiki is Useful](https://wiki.archlinux.org/title/QEMU)<br  />
 [How To Enable Sound and Where I Found Quickemu](https://askubuntu.com/questions/1314825/how-to-enable-sound-in-qemu)<br  />
 [Microphone Pass Through To VM](https://www.reddit.com/r/VFIO/comments/5ffu3s/microphone_in_passed_through_vm/)<br  />
+[USB Passthrough Windows Guest](https://unix.stackexchange.com/questions/250938/qemu-usb-passthrough-windows-guest)
--- main.sh
+++ main.sh
@@ -11,26 +11,27 @@ cdrom_file=
 boot_options="-boot menu=on"
 
 cpu=
-vcpus="2"
+vcpus=2
 cpu_options="-smp ${vcpus}"
-kvm="false"
+kvm=0
 # Default memory allocated with qemu-system-x86_64 appears to be too small and
 # causes kernel panic with linux -m 2G worked not sure how little you can give
 memory="2G"
 memory_options="-m ${memory}"
 
+# video_options="-vga virtio -display gtk,gl=on"
 video_options="-vga virtio -display sdl,gl=on"
-audio_options=
 
-port_forwarding=
-forward_port="2222"
-qemu_monitoring=
-monitor_port="55555"
+port_forwarding=0
+forward_port=2222
+qemu_monitoring=0
+monitor_port=55555
+
+extra_options=
 
 usage() {
 	cat << EOF
-Usage:
-simpleqemu --drive-file example.qcow2 --cdrom example.iso
+Example usage: simpleqemu --drive-file example.qcow2 --cdrom example.iso
 EOF
 }
 
@@ -45,6 +46,10 @@ else
 				usage
 				exit 0
 				;;
+			-e|--extra-options)
+				extra_options="${extra_options} ${2}"
+				shift; shift
+				;;
 			-d|--drive-file)
 				drive_file="${2}"
 				shift; shift
@@ -58,39 +63,36 @@ else
 				shift; shift
 				;;
 			--cpu)
-				cpu="${2}"
-				shift
+				# Only change cpu if it is null
+				cpu="${cpu:-${2}}"
+				shift; shift
 				;;
 			--vcpus)
 				vcpus="${2}"
-				shift
+				shift; shift
 				;;
-			--ram)
+			-m|--ram)
 				memory="${2}"
 				shift; shift
 				;;
-			-s|--sound)
-				audio_options="${2}"
-				shift; shift
-				;;
 			-f|--forward)
-				port_forwarding="true"
+				port_forwarding=1
 				shift
 				;;
 			--monitor)
-				qemu_monitoring="true"
+				qemu_monitoring=1
 				shift
 				;;
-			-P)
+			-P|--monitor-port)
 				monitor_port="${2}"
 				shift; shift
 				;;
-			-p)
+			-p|--forwarding-port)
 				forward_port="${2}"
 				shift; shift
 				;;
 			-k|--enable-kvm)
-				kvm="true"
+				kvm=1
 				cpu="host"
 				shift
 				;;
@@ -103,20 +105,27 @@ else
 	done
 fi
 
-# Options must be set after all options have had a chance to change from
-# command line parameters
-# full_cmd="${qemu_cmd} ${boot_options} ${cpu_options} ${memory_options} ${video_options} -drive file=${drive_file}"
-full_cmd="${qemu_cmd} ${boot_options} ${cpu_options} ${memory_options} ${video_options} -drive file=${drive_file}"
+# Contruct our command from all the options specified
+full_cmd="${qemu_cmd}"
 
-[ "${kvm}" = true ] && full_cmd="${full_cmd} -enable-kvm"
-[ -n "${cpu}" ] && cpu_options="-cpu ${cpu} ${cpu_options}"
+[ -n "${boot_options}" ] && full_cmd="${full_cmd} ${boot_options}"
+[ -n "${cpu_options}" ] && full_cmd="${full_cmd} ${cpu_options}"
+[ -n "${video_options}" ] && full_cmd="${full_cmd} ${video_options}"
+[ -n "${memory_options}" ] && full_cmd="${full_cmd} ${memory_options}"
+
+[ -n "${drive_file}" ] &&
+	full_cmd="${full_cmd} -drive file=${drive_file},format=raw,index=0,media=disk"
 [ -n "${cdrom_file}" ] && full_cmd="${full_cmd} -cdrom ${cdrom_file}"
-[ -n "${audio_options}" ] && full_cmd="${full_cmd} ${audio_options}"
 
-[ "${port_forwarding}" = true ] &&
+[ "${kvm}" -eq 1 ] && full_cmd="${full_cmd} -enable-kvm"
+[ -n "${cpu}" ] && full_cmd="${full_cmd} -cpu ${cpu} ${cpu_options}"
+
+[ "${port_forwarding}" -eq 1 ] &&
 	full_cmd="${full_cmd} -netdev user,id=net0,hostfwd=tcp::${forward_port}-:22 -device e1000,netdev=net0"
-[ "${qemu_monitoring}" = true ] &&
+[ "${qemu_monitoring}" -eq 1 ] &&
 	full_cmd="${full_cmd} -monitor tcp:127.0.0.1:${monitor_port},server,nowait"
 
-printf "QEMU command: %s\n" "${full_cmd}"
+[ -n "${extra_options}" ] && full_cmd="${full_cmd} ${extra_options}"
+
+printf "====\nGenerated QEMU command is:\n%s\n====\n" "${full_cmd}"
 ${full_cmd}
